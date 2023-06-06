e1badf7b9c7ed86d504237bc06609056f7183d44
RodrigoPoliveira <rodrigopoliveira1@gmail.com>
2023-06-05 21:41:49 -0300

    Iniciando projeto

---
+++ DesafiosIrmandadeSwift/DesafiosIrmandadeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafiosIrmandadeSwiftApp.swift
+//  DesafiosIrmandadeSwift
+//
+//  Created by Rodrigo Oliveira on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DesafiosIrmandadeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
